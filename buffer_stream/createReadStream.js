799fe79df0a5ee66e825b65c35bbf53a2f7babdc
Hyeongmin Kim <50308836@hanmail.net>
2021-11-12 08:27:56 +0900

    modify

--- buffer_stream/createReadStream.js
+++ buffer_stream/createReadStream.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./readme3.txt', {highWaterMark: 16});
+const readStream = fs.createReadStream('./readme3.txt', {highWaterMark: 16});   // highWatermark 버퍼의 크기(바이트 단위)를 정할수 있다. 여러번 나누기 위해 16B 로 낮췄다.
 const data = [];
 
-readStream.on('data', (chunk) => {
+readStream.on('data', (chunk) => {  // readStream 은 이벤트를 사용 data, end, error 이벤트를 사용함.
     data.push(chunk);
     console.log('data: ', chunk, chunk.length);
 });
